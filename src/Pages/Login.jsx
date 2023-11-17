93fa99ba776bf5026d16a9b44b9cc0fd06cc7c93
AlitoZzz <alejonegreira@gmail.com>
2023-11-17 12:32:21 -0300

    slices update

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,22 @@
-function Login(){
+import axios from "axios";
 
-    return(
-        <>
-        
-        </>
-    )
+function Login() {
+  const getToken = async () => {
+    const response = await axios({
+      method: "get",
+      url: "http://localhost:3000/tweets",
+    });
+  };
+
+  return (
+    <div className="row">
+      <div className="col-8 bg-success">
+        <h1>div</h1>
+      </div>
+      <div className="col-4 bg-warning">
+        <h2>div</h2>
+      </div>
+    </div>
+  );
 }
-export default Login
+export default Login;
--- src/redux/tweetsSlice.js
+++ src/redux/tweetsSlice.js
@@ -3,8 +3,22 @@ import { createSlice } from "@reduxjs/toolkit";
 const tweetsSlice = createSlice({
   name: "tweets",
   initialState: [],
-  reducers: {},
+  reducers: {
+    setTweets(state = [], action) {
+      state = action.payload;
+    },
+    addTweet(state = [], action) {
+      state = [...state, action.payload];
+    },
+    deleteAll(state = [], action) {
+      state = [];
+    },
+    deleteOne(state = [], action) {
+      state = state.filter((tweet) => tweet._id !== action.payload);
+    },
+  },
 });
 
-const { reducer } = tweetsSlice;
+const { reducer, actions } = tweetsSlice;
+export const { setTweets, addTweet, deleteAll, deleteOne } = actions;
 export default reducer;
