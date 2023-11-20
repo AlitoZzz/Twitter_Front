6575f0097cc61405586dea416710e0a8f249fde0
emzi16 <emilianozapata0638@gmail.com>
2023-11-20 16:27:44 -0300

    UserProfile 3rd Update

--- src/Pages/UserProfile.jsx
+++ src/Pages/UserProfile.jsx
@@ -2,9 +2,10 @@ import axios from "axios";
 import SideBar from "../components/SideBar";
 import TrendingTab from "../components/TrendingTab";
 import { useSelector, useDispatch } from "react-redux";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { setUserProfileData } from "../redux/userProfileSlice";
+import ProfileBody from "../components/ProfileBody";
 
 function UserProfile() {
   const dispatch = useDispatch();
@@ -30,45 +31,9 @@ function UserProfile() {
   return (
     userProfileData && (
       <>
-        {console.log(userProfileData)}
         <div className="d-flex gap-4 text-light vh-100 justify-content-center">
           <SideBar />
-          <div className="d-flex flex-column w-lg-50 profile-body">
-            <div className="d-flex flex-column h-50 border-bottom">
-              <div
-                className="d-flex align-items-center justify-content-start px-3 gap-3"
-                style={{ minHeight: "3.3rem" }}
-              >
-                <Link to="/" className="text-light sticky-top">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="30"
-                    height="30"
-                    fill="currentColor"
-                    className="bi bi-arrow-left-short"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"
-                    />
-                  </svg>
-                </Link>
-                <div className="d-flex flex-column">
-                  <h5 className="m-0">{`${userProfileData.user.firstname} ${userProfileData.user.lastname}`}</h5>
-                  <small className="text-secondary">{`${userProfileData.user.tweets.length} posts`}</small>
-                </div>
-              </div>
-              <div className="bg-image-profile"></div>
-              <div className="h-50">
-                <img
-                  src={userProfileData.user.pfp}
-                  alt=""
-                  className="w-25 pfp-profile"
-                />
-              </div>
-            </div>
-          </div>
+          <ProfileBody />
           <TrendingTab />
         </div>
       </>
---
+++ src/components/ProfileBody.jsx
@@ -0,0 +1,55 @@
+import BackArrow from "./icons/BackArrow";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+function ProfileBody() {
+  const userProfileData = useSelector((state) => state.userProfileData);
+
+  return (
+    <div className="d-flex flex-column profile-body">
+      <div className="d-flex flex-column top">
+        <div
+          className="d-flex align-items-center justify-content-start px-3 gap-3"
+          style={{ minHeight: "3.3rem" }}
+        >
+          <Link to="/" className="text-light sticky-top">
+            <BackArrow />
+          </Link>
+          <div className="d-flex flex-column">
+            <h5 className="m-0">{`${userProfileData.user.firstname} ${userProfileData.user.lastname}`}</h5>
+            <small className="text-secondary">{`${userProfileData.user.tweets.length} posts`}</small>
+          </div>
+        </div>
+        <div className="bg-image-profile"></div>
+        <div className="h-50">
+          <img
+            src={userProfileData.user.pfp}
+            alt=""
+            className="w-25 pfp-profile"
+          />
+          <div className="d-flex flex-column p-3">
+            <div className="d-flex flex-nowrap">
+              <div className="fs-4 fw-bold">{`${userProfileData.user.firstname} ${userProfileData.user.lastname}`}</div>
+              <div className="d-flex gap-3 ms-auto">
+                <span>
+                  19 <span className="text-secondary">Following</span>
+                </span>
+                <span>
+                  123 <span className="text-secondary">Followers</span>
+                </span>
+              </div>
+            </div>
+            <div className="mb-3 text-secondary">{`@${userProfileData.user.username}`}</div>
+            <div className="mb-3">{userProfileData.user.description}</div>
+            <div>
+              <span className="custom-tab pb-3 fw-bold">Posts</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default ProfileBody;
